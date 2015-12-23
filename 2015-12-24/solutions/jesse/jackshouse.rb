7c991c6663bcd45555158a901bf9a707e76c508a
Jesse Abramson <jesse@influitive.com>
2015-12-23 14:10:22 -0500

    fuck yea im great

--- 2015-12-24/solutions/jesse/jackshouse.rb
+++ 2015-12-24/solutions/jesse/jackshouse.rb
@@ -1,41 +1,57 @@
 class JacksHouse
-	def data
-		@data ||= ['the horse and the hound and the horn that belonged to',
-	  'the farmer sowing his corn that kept',
-	  'the rooster that crowed in the morn that woke',
-	  'the priest all shaven and shorn that married',
-	  'the man all tattered and torn that kissed',
-	  'the maiden all forlorn that milked',
-	  'the cow with the crumpled horn that tossed',
-	  'the dog that worried',
-	  'the cat that killed',
-	  'the rat that ate',
-	  'the malt that lay in',
-	  'the house that Jack built']
+	DATA = ['the horse and the hound and the horn that belonged to',
+  'the farmer sowing his corn that kept',
+  'the rooster that crowed in the morn that woke',
+  'the priest all shaven and shorn that married',
+  'the man all tattered and torn that kissed',
+  'the maiden all forlorn that milked',
+  'the cow with the crumpled horn that tossed',
+  'the dog that worried',
+  'the cat that killed',
+  'the rat that ate',
+  'the malt that lay in',
+  'the house that Jack built']
+
+	def initialize(order: Order.new, format: Format.new)
+		@data = order.set(DATA)
+		@format = format
 	end
+	attr_reader :data, :format
 
 	def line number
 		data.last(number)
 	end
 
 	def output
-		(1..data.length).map do |i|
-			puts line(i)
+		(1..data.length).each do |i|
+			puts format.set line(i)
 			puts '-----'
 		end
 	end
 end
 
-class EchoHouse < JacksHouse
-	def line number
-		data.last(number).map do |l|
-			"#{l} #{l}"
-		end
+class Order
+	def set data
+		data
 	end
 end
 
-class RandomHouse < JacksHouse
-	def data
-		@data ||= super.shuffle
+class RandomOrder
+	def set data
+		data.shuffle
+	end
+end
+
+class Format
+	def set line
+		line
+	end
+end
+
+class EchoFormat
+	def set line
+		line.map do |l|
+			"#{l} #{l}"
+		end
 	end
 end
