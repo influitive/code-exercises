55d9dbb47d918ae74bba514b2a6636434d3feb21
Dan Ofer and Eduardo Poleo <dev+dan+eduardo@influitive.com>
2016-01-22 14:02:42 -0500

    Team exercise Friday, January 22, 2016

--- 2016-01-08/solutions/group/bottles/lib/bottles.rb
+++ 2016-01-08/solutions/group/bottles/lib/bottles.rb
@@ -11,14 +11,43 @@ class Bottles
   def verse(number)
     case number
       when 0
-        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-      when 1
-        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-      when 2
-        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+        "No more #{pluralize(number)} of beer on the wall, #{no_more(number + 1)} #{pluralize(number)} of beer.\nGo to the store and buy some more, #{no_more(number)} #{pluralize(number - 1)} of beer on the wall.\n"
       else
-        "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+        "#{number} #{pluralize(number)} of beer on the wall, #{no_more(number + 1)} #{pluralize(number)} of beer.\nTake #{it_or_one(number)} down and pass it around, #{no_more(number)} #{pluralize(number - 1)} of beer on the wall.\n"
     end
   end
 
+  def no_more(number)
+    if number == 0
+      99
+    elsif number == 1
+      "no more"
+    else
+      number - 1
+    end
+  end
+
+  def new_no_more(number)
+    if number == 0
+      "No more"
+    else
+      number
+    end
+  end
+
+  def pluralize(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def it_or_one(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
 end
