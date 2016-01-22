e4ebb1dad7eda9a5e508f0c0ace4fc1bfbeb4206
Dan Ofer and Eduardo Poleo <dev+dan+eduardo@influitive.com>
2016-01-22 13:06:37 -0500

    Revert "wow bottles are so stupid"
    This reverts commit 84e305e31bffc0bf7bed40f85968433b3bb9a214.

--- 2016-01-08/solutions/group/bottles/lib/bottles.rb
+++ 2016-01-08/solutions/group/bottles/lib/bottles.rb
@@ -8,18 +8,16 @@ class Bottles
     upper.downto(lower).map { |i| verse(i) }.join("\n")
   end
 
-  def number_bottles number
-    "#{number} bottle#{number == 1 ? '' : 's'}"
-  end
-
   def verse(number)
     case number
       when 0
         "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
       when 1
         "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      when 2
+        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
       else
-        "#{number_bottles(number)} of beer on the wall, #{number_bottles(number)} of beer.\nTake one down and pass it around, #{number_bottles(number - 1)} of beer on the wall.\n"
+        "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
     end
   end
 
